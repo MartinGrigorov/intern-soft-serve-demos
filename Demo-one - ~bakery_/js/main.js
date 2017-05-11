c9f3f97e6772924b69db580bdab2661dca4cad0d
MartinGrigorov <marto.g.v@gmail.com>
2017-05-11 18:19:39 +0300

    add some comments

--- Demo-one - ~bakery_/js/main.js
+++ Demo-one - ~bakery_/js/main.js
@@ -1,8 +1,12 @@
+
+
+// function that takes the input from the daily menu and calculate price and discount
 function calc() {
     var quantity = Number(document.getElementsByClassName('quantity')[0].value),
-        price = Number(document.getElementsByClassName('single-price')[0].innerHTML);
+        price = Number(document.getElementsByClassName('single-price')[0].innerHTML),
+        newPrice = 0,
 
-    var totalPrice = quantity * price;
+        totalPrice = quantity * price;
 
     if (quantity > 2) {
         newPrice = (quantity * price) / 1.2;
@@ -15,38 +19,59 @@ function calc() {
     } else {
         document.getElementsByClassName('discount')[0].style.visibility = 'hidden';
     }
-    document.getElementsByClassName('total-price')[0].innerHTML = (totalPrice).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
 
+    document.getElementsByClassName('total-price')[0].innerHTML = (totalPrice).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
 }
 
-function generateOrderID(l) {
-    var text = "";
-    var possible = "0123456789";
+//  function that takes char from the string-number times
+function generateOrderID(number) {
+    var text = "",
+        possible = "0123456789-MARTIN";
 
-    for (var i = 0; i < l; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    for (let i = 0; i < number; i++){
+      text += possible.charAt((Math.random() * possible.length));
+    }
 
     return text;
 }
 
+
+// callback function witch invoke generateOrderID function and disable the form for ordering
 function orderID() {
     var orderID = generateOrderID(6);
 
     document.getElementById('orderID').innerHTML = orderID;
+
     document.getElementById('orderButton').disabled = true;
+    document.getElementById('orderButton').setAttribute('style', 'background-color:rgb(217, 237, 226)');
+    document.getElementById('orderButton').innerHTML = 'You already ordered';
     document.getElementById('orderIDp').style.visibility = 'visible';
+    document.getElementById('quantity').disabled = true;
+
 }
 
+// function that takes the input from recipes and calculate quantity for each engridient
 function calculateIngridients() {
-    var qnt = document.getElementById('serving').value;
-    var amounts = document.querySelectorAll('span[data-amount]');
-    var kilos = document.querySelectorAll('kilos');
+    var qnt = document.getElementById('serving').value,
+        amounts = document.querySelectorAll('span[data-amount]'),
+
+        kilos = document.querySelectorAll('kilos');
+
 
     for (let i = 0; i < amounts.length; ++i) {
         var originalAmount = amounts[i].getAttribute('data-amount');
 
         var currentAmount = (originalAmount * qnt) / 5;
         amounts[i].innerHTML = currentAmount;
+
+        for (let i = 0; i < kilos.length; i++) {
+          if (currentAmount > 1000) {
+              kilos[i].innerHTML = 'kilo';
+          }else {
+            kilos[i].innerHTML = 'grams';
+          }
+
+        }
     }
 
 }

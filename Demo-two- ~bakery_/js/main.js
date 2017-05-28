144a744b963a5adfa8c1177f691bd5fa9e0ab3b4
MartinGrigorov <marto.g.v@gmail.com>
2017-05-28 19:20:11 +0300

    switching styles

--- Demo-two- ~bakery_/js/main.js
+++ Demo-two- ~bakery_/js/main.js
@@ -4,9 +4,6 @@ $('#quantity').on('change', function() {
         price = Number($('.single-price').text()),
         newPrice = 0;
 
-        console.log(quantity);
-        console.log(quantity);
-
     totalPrice = (quantity * price).toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");
 
     if (quantity > 2) {
@@ -23,113 +20,102 @@ $('#quantity').on('change', function() {
     $('.total-price').text(totalPrice);
 });
 
-
-
-
 //  function that takes char from the string-number times
 function generateOrderID(number) {
-var text = "",
-    possible = "0123456789-MARTIN";
+    var text = "",
+        possible = "0123456789-MARTIN";
 
-for (let i = 0; i < number; i++) {
-    text += possible.charAt((Math.random() * possible.length));
+    for (let i = 0; i < number; i++) {
+        text += possible.charAt((Math.random() * possible.length));
+    }
+    return text;
 }
-return text;
-}
-
 
 // callback function witch invoke generateOrderID function and disable the form for ordering
-$('#orderButton').click(function(){
+$('#orderButton').click(function() {
 
-  var orderID = generateOrderID(6);
+    var orderID = generateOrderID(6);
 
-  $('#orderID').text(orderID);
+    $('#orderID').text(orderID);
 
-  $(this).attr('disabled','disabled').css('background-color', 'rgb(217, 237, 226)' ).text('You already ordered');
-  $('#orderIDp').css('visibility', 'visible');
-  $('#quantity').attr('disabled','disabled');
+    $(this).attr('disabled', 'disabled').css('background-color', 'rgb(217, 237, 226)').text('You already ordered');
+    $('#orderIDp').css('visibility', 'visible');
+    $('#quantity').attr('disabled', 'disabled');
 });
-
-
 
 // function that takes the input from recipes and calculate quantity for each engridient
-$("#serving").on('change', function(){
-  var qnt = $(this).val();
-  $('.amount').each(function(index){
-    var originalAmount = $(this).attr('data-amount');
-    console.log(originalAmount);
-    var currentAmount = (originalAmount * qnt) / 5;
-    $(this).text( currentAmount );
-  });
+$("#serving").on('change', function() {
+    var qnt = $(this).val();
+    $('.amount').each(function(index) {
+        var originalAmount = $(this).attr('data-amount');
+        console.log(originalAmount);
+        var currentAmount = (originalAmount * qnt) / 5;
+        $(this).text(currentAmount);
+    });
 });
 
-
-
-  $('ul#filter a').click(function() {
-    $(this).css('outline','none');
+$('ul#filter a').click(function() {
+    $(this).css('outline', 'none');
     $('ul#filter .current').removeClass('current');
     $(this).parent().addClass('current');
 
-    var filterVal = $(this).text().toLowerCase().replace(' ','-');
+    var filterVal = $(this).text().toLowerCase().replace(' ', '-');
 
-    if(filterVal == 'all') {
-      $('#portfolio .recipe-item.hidden').fadeIn('slow').removeClass('hidden');
+    if (filterVal == 'all') {
+        $('#portfolio .recipe-item.hidden').fadeIn('slow').removeClass('hidden');
     } else {
-      $('#portfolio .recipe-item').each(function() {
-        if(!$(this).hasClass(filterVal)) {
-          $(this).fadeOut('slow').addClass('hidden');
-        } else {
-          $(this).fadeIn('slow').removeClass('hidden');
-        }
-      });
+        $('#portfolio .recipe-item').each(function() {
+            if (!$(this).hasClass(filterVal)) {
+                $(this).fadeOut('slow').addClass('hidden');
+            } else {
+                $(this).fadeIn('slow').removeClass('hidden');
+            }
+        });
     }
 
     return false;
-  });
-
-
-function cycleImages(){
-      var $active = $('.sliderWrapper .active');
-      var $next = ($active.next().length > 0) ? $active.next() : $('.sliderWrapper img:first');
-      $next.css('z-index',2);//move the next image up the pile
-      $active.fadeOut(1500,function(){//fade out the top image
-	  $active.css('z-index',1).show().removeClass('active');//reset the z-index and unhide the image
-          $next.css('z-index',3).addClass('active');//make the next image the top one
-      });
-    }
-
-$(document).ready(function(){
-setInterval('cycleImages()', 2500);
 });
 
-
-
-function myDate(){
-  var now = new Date();
-  var hours = now.getHours();
-
-  if( hours > 7 && hours < 22 ){
-    if( hours > 7 && hours < 11 ){
-    $('#lunch-btn, .item-lunch').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
-    $('#dinner-btn, .item-dinner').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
-    }
-    if( hours > 11 && hours < 17 ){
-    $('#breakfast-btn, .item-breakfast').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
-    $('#dinner-btn, .item-dinner').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
-    }
-    if( hours > 17 && hours < 22 ){
-      $('#breakfast-btn, .item-breakfast').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
-      $('#lunch-btn, .item-lunch').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
-    }
-  }
-
-  else{
-    $('#breakfast-btn, .item-breakfast').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
-    $('#lunch-btn, .item-lunch').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
-    $('#dinner-btn, .item-dinner').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
-  }
+function cycleImages() {
+    var $active = $('.sliderWrapper .active');
+    var $next = ($active.next().length > 0)
+        ? $active.next()
+        : $('.sliderWrapper img:first');
+    $next.css('z-index', 2); //move the next image up the pile
+    $active.fadeOut(1500, function() { //fade out the top image
+        $active.css('z-index', 1).show().removeClass('active'); //reset the z-index and unhide the image
+        $next.css('z-index', 3).addClass('active'); //make the next image the top one
+    });
 }
 
-$(document).ready(function(){
-  myDate();
+$(window).on('load', function() {
+    setInterval('cycleImages()', 2500);
+});
+
+function myDate() {
+    var now = new Date();
+    var hours = now.getHours();
+
+    if (hours > 7 && hours < 22) {
+        if (hours > 7 && hours < 11) {
+            $('#lunch-btn, .item-lunch').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
+            $('#dinner-btn, .item-dinner').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
+        } else if (hours > 11 && hours < 17) {
+            $('#breakfast-btn, .item-breakfast').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
+            $('#dinner-btn, .item-dinner').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
+        } else if (hours > 17 && hours < 22) {
+            $('#breakfast-btn, .item-breakfast').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
+            $('#lunch-btn, .item-lunch').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
+        }
+    } else {
+        $('#breakfast-btn, .item-breakfast').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
+        $('#lunch-btn, .item-lunch').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
+        $('#dinner-btn, .item-dinner').attr('data-toggle', 'modal').attr('data-target', '#error').attr('href', '#');
+        $('#if-close').html("Our Bakery-restaurant is closed, you can order from <br>8 O'clock till 21 O'clock");
+        $('#if-close').removeClass('alert-info').addClass('alert-danger');
+    }
+}
+
+$(document).ready(function() {
+    myDate();
 });
